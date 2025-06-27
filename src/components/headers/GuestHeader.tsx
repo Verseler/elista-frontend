81021bdfdff9401987c9de7f06747d9c7c31b6a6
Verseler <vrslr2003@gmail.com>
2025-06-27 22:45:52 +0800

    fix: fix routing issue based on user authentication status and role

--- src/components/headers/GuestHeader.tsx
+++ src/components/headers/GuestHeader.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router";
 import { Button } from "@/components/ui/button";
 import Container from "@/components/ui/container";
-import AppLogo from "../ui/app-logo";
+import AppLogo from "@/components/ui/app-logo";
+import { useAuth } from "@/hooks/useAuth";
+import type { UserRole } from "@/types";
 
 export default function GuestHeader() {
+  const { isAuthenticated, user } = useAuth();
+
   return (
     <nav className="bg-white/80 backdrop-blur-md shadow-sm sticky top-0 z-50">
       <Container>
@@ -11,15 +15,48 @@ export default function GuestHeader() {
           <AppLogo />
 
           <div className="ml-auto space-x-2">
-            <Button variant="ghost" asChild>
-              <Link to="/login">Sign In</Link>
-            </Button>
-            <Button className="bg-primary-600 hover:bg-primary-700" asChild>
-              <Link to="/register">Get Started</Link>
-            </Button>
+            <GuestHeaderNav
+              isAuthenticated={isAuthenticated}
+              role={user?.role || "borrower"}
+            />
           </div>
         </div>
       </Container>
     </nav>
+  );
+}
+
+type GuestHeaderNavProps = {
+  isAuthenticated: boolean;
+  role: UserRole;
+};
+
+function GuestHeaderNav({ isAuthenticated, role }: GuestHeaderNavProps) {
+  if (!isAuthenticated) {
+    return (
+      <>
+        <Button variant="ghost" asChild>
+          <Link to="/login">Sign In</Link>
+        </Button>
+        <Button className="bg-primary-600 hover:bg-primary-700" asChild>
+          <Link to="/register">Get Started</Link>
+        </Button>
+      </>
+    );
+  }
+
+  if (role === "store_owner") {
+    return (
+      <Button className="bg-primary-600 hover:bg-primary-700" asChild>
+        <Link to="/store-owner">Dashboard</Link>
+      </Button>
+    );
+  }
+
+  //borrower
+  return (
+    <Button className="bg-primary-600 hover:bg-primary-700" asChild>
+      <Link to="/borrower">Dashboard</Link>
+    </Button>
   );
 }
